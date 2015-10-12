7d7c5f8e9a4e82981efeea0535e575ab0c880e53
Yad Smood <y.s.inside@gmail.com>
2015-10-12 13:31:23 +0800

    opt: build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
-module.exports = {
+var webpack = require("webpack");
+var isProduction = process.env.NODE_ENV === "production";
+
+var self = module.exports = {
     entry: {
         "test-browser": "./test/test-browser.js",
-        "browser": "./src/browser"
+        "yaku.browser.full": "./src/browser"
     },
 
     output: {
         filename: "[name].js",
-        path: "./lib"
-    }
+        path: "./dist",
+        pathinfo: true
+    },
+
+    debug: true
 };
+
+if (isProduction) {
+    self.output.filename = "[name].min.js";
+    self.plugins = [new webpack.optimize.UglifyJsPlugin()];
+}
