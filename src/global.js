496f0be916f9dcadb4d640b292df1dfa97e67de5
Yad Smood <y.s.inside@gmail.com>
2017-04-20 17:20:20 +0800

    Revert "fix: #45"
    b86aad16f696cf696703feb4e795aa1f0df26a32

--- src/global.js
+++ src/global.js
@@ -1,8 +1,7 @@
 var Yaku = require("./yaku");
 
 try {
-    // TODO: #45 revert it in the future
-    (global||{}).Promise = Yaku;
+    global.Promise = Yaku;
     window.Promise = Yaku;
 } catch (err) {
     null;
