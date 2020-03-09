c464c022cdc13e2e1b4772eeda14d1a56671bc41
Yad Smood <y.s.inside@gmail.com>
2020-03-10 02:47:48 +0900

    fix #56 global.Promise should be non-enumerable and should have correct name property

--- src/global.js
+++ src/global.js
@@ -1,8 +1,19 @@
 var Yaku = require('./yaku');
 
+var root = (typeof global === 'undefined') ? window : global;
+
 try {
-    global.Promise = Yaku;
-    window.Promise = Yaku;
+    root.Promise = Yaku;
+
+    Object.defineProperty(root, 'Promise', {
+        value: Yaku,
+        enumerable: false
+    });
+
+    Object.defineProperty(Promise, 'name', {
+        value: 'Promise',
+        enumerable: false
+    });
 } catch (err) {
     null;
 }
